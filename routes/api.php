b4c415464c03375a0f8c213d0dae9957b8825c6c
Fatouma fofana <fatouma53@live.fr>
2023-12-06 14:54:26 +0100

    routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
+namespace App\Routes;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CommentController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +21,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', [CommentController::class, 'index']);
+Route::get('/comments', [CommentController::class, 'fetchComments']);
+Route::post('/comments', [CommentController::class, 'store']);
